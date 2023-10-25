7e57aed74fbd7560c26ece6125b1f949e7acb7ef
Anders Magnusson <anders14magnusson@gmail.com>
2023-10-25 20:08:00 +0200

    Is today Groundhog's Day?

---
+++ src/theme.ts
@@ -0,0 +1,21 @@
+import { createTheme } from "@mui/material/styles"
+import { blue, purple } from "@mui/material/colors"
+
+// A custom theme for this app
+const theme = createTheme({
+    palette: {
+        mode: "dark",
+        primary: {
+            main: blue[200],
+            light: blue[50],
+            dark: blue[400],
+        },
+        secondary: {
+            main: purple["200"],
+            light: purple["50"],
+            dark: purple["400"],
+        },
+    },
+})
+
+export default theme
