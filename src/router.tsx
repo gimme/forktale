25a5bce657c4566e9481847c159379e99448e395
Anders Magnusson <anders14magnusson@gmail.com>
2023-10-29 00:26:20 +0200

    feat: Add error page

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,16 @@
+import React from "react"
 import { createBrowserRouter } from "react-router-dom"
 import App from "@/App.tsx"
+import ErrorPage from "@/ErrorPage.tsx"
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-    },
-])
+const router = createBrowserRouter([route("/", <App />)])
+
+function route(path: string, element: React.ReactNode) {
+    return {
+        path,
+        element,
+        errorElement: <ErrorPage />,
+    }
+}
 
 export default router
